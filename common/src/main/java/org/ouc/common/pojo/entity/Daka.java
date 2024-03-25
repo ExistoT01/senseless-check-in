5b4d172756e2271030ff7e97752a323c73dd7045
双 林 <l17305042002@163.com>
2024-03-25 21:04:29 +0800

    人流量查询(新）

---
+++ common/src/main/java/org/ouc/common/pojo/entity/Daka.java
@@ -0,0 +1,16 @@
+package org.ouc.common.pojo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Daka {
+    private String startTime;
+    private String endTime;
+    private String time;
+
+}
