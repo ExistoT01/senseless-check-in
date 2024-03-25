b2ace7892e4ce1c82145d4bd125f88503ae0e6d7
双 林 <l17305042002@163.com>
2024-03-25 17:39:37 +0800

    人流量查询

---
+++ common/src/main/java/org/ouc/common/pojo/entity/Liuliang.java
@@ -0,0 +1,12 @@
+package org.ouc.common.pojo.entity;
+
+
+public class Liuliang {
+    private String name;
+    private String idNumber;
+    private String purp; // 打卡目的
+    private String gender;
+    private String sec; // 部门
+
+    // 构造方法、getter和setter省略...
+}
