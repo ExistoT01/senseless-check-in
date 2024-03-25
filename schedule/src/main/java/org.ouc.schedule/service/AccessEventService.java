6f98210f67a637758230357fc36fc04b919947f2
双 林 <l17305042002@163.com>
2024-03-25 17:25:16 +0800

    增加定时模块统计功能（按日、周、月进行出入统计）

--- schedule/src/main/java/org.ouc.schedule/service/AccessEventService.java
+++ schedule/src/main/java/org.ouc.schedule/service/AccessEventService.java
@@ -1,10 +1,15 @@
 package org.ouc.schedule.service;
 
+import org.ouc.common.pojo.entity.Liuliang;
 import org.ouc.common.result.Result;
 import org.ouc.common.pojo.entity.AccessEvent;
 import org.ouc.schedule.mapper.AccessEventMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Service
 public class AccessEventService {
@@ -25,4 +30,13 @@ public class AccessEventService {
         return Result.success(accessEventMapper.getTotalAccessCountByMonth(date));
     }
 
+
+    public int liuliang(String date1, String date2) {
+        return accessEventMapper.liuliang(date1, date2);
+    }
+
+
+    public List<Liuliang> liuliangs(String date1, String date2) {
+        return accessEventMapper.liuliangs(date1, date2);
+    }
 }
